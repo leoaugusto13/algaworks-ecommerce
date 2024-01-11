3dca34ca12a3854edf66fc870d5a126ecf62cc05
Leonardo <leo,augustobh13@gmail.com>
2024-01-11 16:05:28 -0300

    first commit

---
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -0,0 +1,66 @@
+package com.algaworks.ecommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Produto {
+
+    @Id
+    private Integer Id;
+
+    private String descricao;
+
+    private String nome;
+
+    private BigDecimal preco;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Produto produto = (Produto) o;
+
+        return Id.equals(produto.Id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Id.hashCode();
+    }
+}
