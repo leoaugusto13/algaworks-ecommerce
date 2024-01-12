1d623b9a10d834ceb786d78ad24e39adf7c41dad
Leonardo <leo,augustobh13@gmail.com>
2024-01-12 12:07:21 -0300

    create method insertObject

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperaçoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperaçoesComTransacaoTest.java
@@ -2,9 +2,31 @@ package com.algaworks.ecommerce.iniciandocomjpa;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Produto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 public class OperaçoesComTransacaoTest extends EntityManagerTest {
+
+    @Test
+    public void insertObject(){
+
+        Produto produto = new Produto();
+        produto.setId(2);
+        produto.setNome("Teclado RedDragon");
+        produto.setDescricao("Teclado mecânico RedDragon para Games");
+        produto.setPreco(new BigDecimal(160.00));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assertions.assertNotNull(produtoVerificacao);
+    }
 
     @Test
     public void openAndCloseTransaction(){
