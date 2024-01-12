aa5de29913a454646577cfdd4553d9fc36095553
Leonardo <leo,augustobh13@gmail.com>
2024-01-12 11:41:06 -0300

    alter unit test

---
+++ src/test/java/com/algaworks/ecommerce/EntityManagerTest.java
@@ -0,0 +1,35 @@
+package com.algaworks.ecommerce;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+public class EntityManagerTest {
+
+    protected static EntityManagerFactory entityManagerFactory;
+
+    protected EntityManager entityManager;
+
+    @BeforeAll()
+    public static void setupBeforeAll(){
+        entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
+    }
+
+    @AfterAll
+    public static void tearDownAfterAll(){
+        entityManagerFactory.close();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        entityManagerFactory.close();
+    }
+
+
+}
